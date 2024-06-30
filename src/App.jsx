c53b6e043cca13b607bcd5d134beeac57e2e7730
Jacob <jakevilevac@gmail.com>
2024-06-29 17:10:36 -0700

    case where no category and minor styling, prepping for tag system

--- src/App.jsx
+++ src/App.jsx
@@ -7,51 +7,52 @@ import ScrollToTopButton from './components/ScrollToTopButton';
 import { useItems } from './hooks/useItems';
 
 const initialSelectedCategories = {
-	'Weapons': true,
-	'Spells': true,
-	'Ashes of War': true,
-	'Spirit Ashes': true,
-	'Talismans': true,
-	'Tears': true,
-	'Tools': true,
-	'Armor': true,
+  'Weapons': true,
+  'Spells': true,
+  'Ashes of War': true,
+  'Spirit Ashes': true,
+  'Talismans': true,
+  'Tears': true,
+  'Tools': true,
+  'Armor': true,
 };
 
 function App() {
-	const {
-		items,
-		checkedItems,
-		filters,
-		showScroll,
-		handleCheck,
-		handleFilterChange,
-		filteredItems,
-		scrollToTop,
-		counts
-	} = useItems(initialSelectedCategories);
+  const {
+    items,
+    checkedItems,
+    filters,
+    showScroll,
+    handleCheck,
+    handleFilterChange,
+    filteredItems,
+    scrollToTop,
+    counts,
+  } = useItems(initialSelectedCategories);
 
-    const isAnyCategorySelected = Object.values(filters.selectedCategories).some(selected => selected);
+  const isAnyCategorySelected = Object.values(filters.selectedCategories).some((selected) => selected);
 
-	return (
-		<div className="App">
-			<div className="sidebar">
-				<Filter filters={filters} onFilterChange={handleFilterChange} />
-				<div className="counters">
-					{Object.keys(counts).map(category => (
-						<ItemCounter key={category} category={category} count={counts[category]} />
-					))}
-				</div>
-			</div>
-			<div className="content">
-                {!isAnyCategorySelected && <p className="no-category-message">Select an Item Category on the Left</p>}
-                {isAnyCategorySelected && (
-                <>
-                    <ItemList items={filteredItems} checkedItems={checkedItems} onCheck={handleCheck} />
-                    <ScrollToTopButton showScroll={showScroll} onClick={scrollToTop} />
-                </>)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <Filter filters={filters} onFilterChange={handleFilterChange} />
+      </div>
+      <div className="content">
+        <div className="counters">
+          {Object.keys(counts).map((category) => (
+            <ItemCounter key={category} category={category} count={counts[category]} />
+          ))}
+        </div>
+        {!isAnyCategorySelected && <p className="no-category-message">Select an Item Category on the Left</p>}
+        {isAnyCategorySelected && (
+          <>
+            <ItemList items={filteredItems} checkedItems={checkedItems} onCheck={handleCheck} />
+            <ScrollToTopButton showScroll={showScroll} onClick={scrollToTop} />
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/add_tags.py
@@ -0,0 +1,42 @@
+import json
+
+# List of JSON files to process
+files = [
+    'armor.json',
+    'ashes_of_war.json',
+    'spells.json',
+    'spirit_ashes.json',
+    'talismans.json',
+    'tears.json',
+    'tools.json',
+    'weapons.json'
+]
+
+def generate_tags(item):
+    tags = []
+    if 'spoiler' in item and item['spoiler']:
+        tags.append('spoiler')
+    if 'location_text' in item:
+        if 'Remembrance' in item['location_text'] or 'Purchased at Roundtable Hold' in item['location_text']:
+            tags.append('boss')
+        if 'quest' in item['location_text'].lower():
+            tags.append('quest')
+        if 'missable' in item['location_text'].lower():
+            tags.append('missable')
+    return tags
+
+# Process each file
+for file in files:
+    with open(file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    for item in data:
+        item['tags'] = generate_tags(item)
+        if 'pieces' in item and item['pieces']:
+            for piece in item['pieces']:
+                piece['tags'] = generate_tags(piece)
+    
+    with open(file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+print("Tags added successfully to all items.")
