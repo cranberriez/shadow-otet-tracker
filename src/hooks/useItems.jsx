8feed91140d885c27b4b29b6fe28520cc1d9d0b4
Jacob <jakevilevac@gmail.com>
2024-07-02 19:34:07 -0700

    fixed a bug with selecting an item piece

--- src/hooks/useItems.jsx
+++ src/hooks/useItems.jsx
@@ -64,22 +64,20 @@ export const useItems = (initialSelectedCategories) => {
             }
 
             // If the item is a piece, check if all pieces are now checked and mark the set as checked
-            if (item.pieces) {
-                if (!item || (item && item.pieces.length === 0)) {
-                    const setItemId = Math.floor(id);
-                    const setItem = items.find(item => item.id === setItemId);
-                    if (setItem && setItem.pieces) {
-                        const allPiecesChecked = setItem.pieces.every(piece =>
-                            piece.id === id ? newCheckedItems[piece.id] : prevCheckedItems[piece.id]
-                        );
-                        newCheckedItems[setItemId] = allPiecesChecked;
-                    }
-                } else {
-                    // If the item is a set, uncheck the set if any piece is unchecked
-                    const setItemId = item.id;
-                    const allPiecesChecked = item.pieces.every(piece => newCheckedItems[piece.id]);
+            if (!item || (item && item.pieces.length === 0)) {
+                const setItemId = Math.floor(id);
+                const setItem = items.find(item => item.id === setItemId);
+                if (setItem && setItem.pieces) {
+                    const allPiecesChecked = setItem.pieces.every(piece =>
+                        piece.id === id ? newCheckedItems[piece.id] : prevCheckedItems[piece.id]
+                    );
                     newCheckedItems[setItemId] = allPiecesChecked;
                 }
+            } else {
+                // If the item is a set, uncheck the set if any piece is unchecked
+                const setItemId = item.id;
+                const allPiecesChecked = item.pieces.every(piece => newCheckedItems[piece.id]);
+                newCheckedItems[setItemId] = allPiecesChecked;
             }
 
             return newCheckedItems;
